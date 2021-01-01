b4361fc36224c8eae2ca080d4501e02a7febe364
damian <damian@example.com>
2021-01-01 14:57:31 +0100

    dziala

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import queue ,random
 import psycopg2
 import threading
 
+
 q = queue.Queue()
 
 class DBManager:
@@ -16,42 +17,14 @@ class DBManager:
     def __del__(self):
         self.conn.close()
 
-    def internet_database(self):
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute("DROP TABLE IF EXISTS mobile")
-            create_table_query = '''CREATE TABLE mobile
-                      (ID INT PRIMARY KEY     NOT NULL,
-                      MODEL           TEXT    NOT NULL,
-                      PRICE         REAL); '''
-            # Execute a command: this creates a new table
-            cursor.execute(create_table_query)
-            self.conn.commit()
-            print("Table created successfully in PostgreSQL ")
-            insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (1, 'Iphone12', 1100)"""
-            cursor.execute(insert_query)
-            postgreSQL_select_Query = "select * from mobile where id = %s"
-            mobileID = 1
-            cursor.execute(postgreSQL_select_Query, (mobileID,))
-            mobile_records = cursor.fetchall()
-            # print(mobile_records)
-
-            self.conn.commit()
-
-        except (Exception, psycopg2.DatabaseError) as error:
-            print("Error while connecting to PostgreSQL", error)
 
     def delete_db(self):
         try:
             cur = self.conn.cursor()
             cur.execute("DROP TABLE IF EXISTS sweets CASCADE")
-            print(cur.rowcount)
             cur.execute("DROP TABLE IF EXISTS packs CASCADE")
-            print(cur.rowcount)
             cur.execute("DROP TABLE IF EXISTS pack_sweet CASCADE")
-            print(cur.rowcount)
             cur.execute("DROP TABLE IF EXISTS sweet_resemblance CASCADE")
-            print(cur.rowcount)
             self.conn.commit()
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
@@ -59,10 +32,10 @@ class DBManager:
 
     def create_db(self):
         commands = ("""CREATE TABLE IF NOT EXISTS sweets (
-                        my_name text ,
-                        number_left int,
-                        more_than_0 int check (number_left > 0),
-                        PRIMARY KEY(my_name)
+                        name text ,
+                        has_left int,
+                        more_than_0 int check (has_left > 0),
+                        PRIMARY KEY(name)
                     );""",
                     """CREATE TABLE IF NOT EXISTS packs (
                         id int,
@@ -75,8 +48,8 @@ class DBManager:
                         sweet text,
                         number int,
                         CONSTRAINT fk_pack_id
-                            FOREIGN KEY(pack_id)
-                                REFERENCES packs(id)
+                        FOREIGN KEY(pack_id)
+                        REFERENCES packs(id)
                     );""",
                     """CREATE TABLE IF NOT EXISTS sweet_resemblance (
                         sweet_1 text,
@@ -84,10 +57,10 @@ class DBManager:
                         resemblance decimal,
                         CONSTRAINT fk_sweet_1
                         FOREIGN KEY(sweet_1)
-                        REFERENCES sweets(my_name),
+                        REFERENCES sweets(name),
                         CONSTRAINT fk_sweet_2
                         FOREIGN KEY(sweet_2)
-                        REFERENCES sweets(my_name)
+                        REFERENCES sweets(name)
                     );""")
 
         try:
@@ -102,15 +75,14 @@ class DBManager:
 
 
     def fill_db(self, sweets_list, resemblance_list):
-        sweets_insert_command = ("INSERT INTO sweets(my_name, number_left, more_than_0) VALUES(%s, %s, %s);")
-        resemblance_insert_command = ("""INSERT INTO sweet_resemblance(sweet1, sweet2, resemblance) VALUES(%s, %s, %s);""")
+        sweets_insert_command = ("INSERT INTO sweets(name, has_left, more_than_0) VALUES(%s, %s, %s);")
+        resemblance_insert_command = ("""INSERT INTO sweet_resemblance(sweet_1, sweet_2, resemblance) VALUES(%s, %s, %s);""")
 
-        print(sweets_list)
         try:
             cur = self.conn.cursor()
 
             cur.executemany(sweets_insert_command, sweets_list)
-            # cur.executemany(resemblance_insert_command, resemblance_list)
+            cur.executemany(resemblance_insert_command, resemblance_list)
             cur.close()
             self.conn.commit()
 
@@ -131,46 +103,37 @@ class DBManager:
     def do_transaction(self, id, country, recipent, sweets_list):
         try:
             # todo change me
-            self.set_isolation_level("SERIALIZABLE")
+            self.set_isolation_level("READ_UNCOMMITTED")
 
             cur = self.conn.cursor()
 
-            print("HELLO")
-            postgreSQL_select_Query = "select * from mobile where id = %s"
-            mobileID = 1
-            cur.execute(postgreSQL_select_Query, (mobileID,))
-            mobile_records = cur.fetchall()
-            print(mobile_records)
-
-
-            cur.execute("INSERT INTO packs(id, country, recipent) VALUES(%s, %s, %s)", (id, country, recipent));
+            cur.execute("INSERT INTO packs(id, country, recipent) VALUES(%s, %s, %s)", (id, country, recipent))
             assert (cur.rowcount == 1)
 
             # przyjmujemy zalozenie, ze wysylamy paczki jednego typu
             for sweet in sweets_list:
                 found_sweet = False
 
-                has_left = 100
-                cur.execute("SELECT * FROM sweets WHERE number_left = %s;", (has_left,))
-                sweets_in_magazine = cur.fetchone()
+                cur.execute("SELECT has_left FROM sweets WHERE name = %s;", (sweet["name"],))
+                sweets_in_magazine = cur.fetchone()[0]
 
                 # Mamy wlasciwy slodycz
                 if sweets_in_magazine >= sweet["number"]:
-                    cur.execute("INSERT INTO pack_sweet (%s, %s, %s)", id, similar_sweet, sweet["number"])
-                    cur.execute("UPDATE sweets SET number_left = number_left - %s WHERE my_name = %s", sweet["number"], sweet["name"])
+                    cur.execute("INSERT INTO pack_sweet(pack_id, sweet, number) values (%s, %s, %s)", (id, sweet["name"], sweet["number"]))
+                    cur.execute("UPDATE sweets SET has_left = has_left - %s WHERE name = %s", (sweet["number"], sweet["name"]))
                     continue
 
                 # Szukamy podobnego slodycza
-                cur.execute("SELECT sweet_2 FROM sweet_resemblance WHERE sweet_1 == %s ORDER BY resemblance DESC", sweet["name"])
+                cur.execute("SELECT sweet_2 FROM sweet_resemblance WHERE sweet_1 = %s ORDER BY resemblance DESC", (sweet["name"],))
                 similar_sweets = cur.fetchall()
 
                 for similar_sweet in similar_sweets:
-                    cur.execute("SELECT number_left FROM sweets WHERE my_name = %s", similar_sweet)
-                    similar_sweet_in_magazine = cur.fetchone()
+                    cur.execute("SELECT has_left FROM sweets WHERE name = %s", (similar_sweet,))
+                    similar_sweet_in_magazine = cur.fetchone()[0]
                     if similar_sweet_in_magazine > sweet["number"]:
                         found_sweet = True
-                        cur.execute("INSERT INTO pack_sweet (%s, %s, %s)", id, similar_sweet, sweet["number"])
-                        cur.execute("UPDATE sweets SET number_left = number_left - %s WHERE my_name = %s", sweet["number"], similar_sweet)
+                        cur.execute("INSERT INTO pack_sweet(pack_id, sweet, number) values (%s, %s, %s)", (id, similar_sweet, sweet["number"]))
+                        cur.execute("UPDATE sweets SET has_left = has_left - %s WHERE name = %s", (sweet["number"], similar_sweet))
                         break
 
                 if not found_sweet:
@@ -187,14 +150,14 @@ class DBManager:
 class DataGenerator:
     def __init__(self):
         self.sweets = ["chocolate", "lollypop", "biscuit", "cake"]
-        self.countries = ["Poland, Germany, France, UK, USA, Norway, Sweden"]
+        self.countries = ["Poland", "Germany", "France", "UK", "USA", "Norway", "Sweden"]
         self.names = ["Damian", "John", "Steve", "Mary", "Anne", "Jane"]
 
     def get_sweets(self):
         sweets_list = []
-        sweets_list.append(('chocolate', 100, 1,))
+        sweets_list.append(('chocolate', 5, 1,))
         sweets_list.append(('lollypop', 100, 1,))
-        sweets_list.append(('biscuit', 100, 1,))
+        sweets_list.append(('biscuit', 5, 1,))
         sweets_list.append(('cake', 100, 1,))
         return sweets_list
 
@@ -214,7 +177,7 @@ class DataGenerator:
         for i in range (1, 1000):
             l = []
             items_on_list = random.randrange(4)
-            for _ in range (1, 3):
+            for _ in range (1, items_on_list):
                 l.append({"number": random.randrange(20), "name": random.choice(self.sweets)})
             q.put({"id": i, "country": random.choice(self.countries), "recipient": random.choice(self.names), "list": l})
 
@@ -225,12 +188,10 @@ def worker():
 
 
 if __name__ == '__main__':
-    print(psycopg2.__version__)
 
     manager = DBManager()
     data_generator = DataGenerator()
 
-    manager.internet_database()
     manager.delete_db()
     manager.create_db()
     manager.fill_db(data_generator.get_sweets(), data_generator.get_resemblance())
@@ -239,7 +200,7 @@ if __name__ == '__main__':
 
     worker()
     # threads = []
-    # for _ in range(1, 2):
+    # for _ in range(1, 5):
     #     t = threading.Thread(target=worker)
     #     threads.append(t)
     #     t.start()
